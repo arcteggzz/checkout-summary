558e0293deab55be00789b9f8dd84920d0568b4a
Oghenetega Esedere <arcteggzz@gmail.com>
2023-01-12 00:58:21 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Our sample H1 text</h1>
+      <div className="card">
+        <button>Apply PromoCode</button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
